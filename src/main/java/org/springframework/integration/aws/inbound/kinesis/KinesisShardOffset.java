e7471ce613060723f5d07cba3e30724abf4abc36
Matthias Wesolowski <mw@wedecom-consulting.de>
2020-06-12 09:21:27 +0200

    remove copyright fixes

--- src/main/java/org/springframework/integration/aws/inbound/kinesis/KinesisShardOffset.java
+++ src/main/java/org/springframework/integration/aws/inbound/kinesis/KinesisShardOffset.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,6 +28,7 @@ import com.amazonaws.services.kinesis.model.ShardIteratorType;
  * A model to represent a sequence in the shard for particular {@link ShardIteratorType}.
  *
  * @author Artem Bilan
+ * @author Matthias Wesolowski
  * @since 1.1
  */
 public class KinesisShardOffset {
--- src/test/java/org/springframework/integration/aws/inbound/KinesisMessageDrivenChannelAdapterTests.java
+++ src/test/java/org/springframework/integration/aws/inbound/KinesisMessageDrivenChannelAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,6 +73,7 @@ import com.amazonaws.services.kinesis.model.Shard;
 
 /**
  * @author Artem Bilan
+ * @author Matthias Wesolowski
  * @since 1.1
  */
 @SpringJUnitConfig
