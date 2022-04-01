d818175db087b37c90b712aef3c552d9a88db254
Artem Bilan <abilan@vmware.com>
2022-04-01 10:02:40 -0400

    Fix `@since` in the `LocalstackContainerTest`

--- src/test/java/org/springframework/integration/aws/LocalstackContainerTest.java
+++ src/test/java/org/springframework/integration/aws/LocalstackContainerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 the original author or authors.
+ * Copyright 2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import com.amazonaws.services.kinesis.AmazonKinesisAsyncClientBuilder;
  *
  * @author Artem Bilan
  *
- * @since 5.5.5
+ * @since 3.0
  */
 @Testcontainers(disabledWithoutDocker = true)
 public interface LocalstackContainerTest {
