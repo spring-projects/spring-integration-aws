7ca09d89c24b15953e876a79aba11d9895575629
Artem Bilan <abilan@vmware.com>
2022-05-17 16:04:40 -0400

    Revert attempts for Harbor Docker mirror

--- src/test/java/org/springframework/integration/aws/LocalstackContainerTest.java
+++ src/test/java/org/springframework/integration/aws/LocalstackContainerTest.java
@@ -43,7 +43,7 @@ public interface LocalstackContainerTest {
 	@Container
 	LocalStackContainer localStack =
 			new LocalStackContainer(
-					DockerImageName.parse(TestUtils.dockerRegistryFromEnv() + "localstack")
+					DockerImageName.parse(TestUtils.dockerRegistryFromEnv() + "localstack/localstack:0.14.2")
 							.asCompatibleSubstituteFor("localstack/localstack"))
 					.withServices(
 							LocalStackContainer.Service.DYNAMODB,
